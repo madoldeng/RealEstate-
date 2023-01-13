17b8299b3fd4029dabf54864537943824f38913b
madoldeng <r_madol@yahoo.com>
2023-01-12 22:06:05 -0500

    Revert "Adding backend code"
    This reverts commit 233b007148d737d7a957af0622c0738a3df3dacc.

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,5 +1,4 @@
 var detailsBox = document.getElementById('details-box');
-const states = document.querySelectorAll("path");
 
 document.addEventListener('mouseover', function (e) {
  if (e.target.tagName == 'path') {
@@ -12,23 +11,13 @@ document.addEventListener('mouseover', function (e) {
  }
 });
 
+button.addEventListener('click', function () {
+ alert('Button clicked!');
+});
+
 window.onmousemove = function (e) {
  var x = e.clientX,
   y = e.clientY;
  detailsBox.style.top = (y + 20) + 'px';
  detailsBox.style.left = (x) + 'px';
 };
-
-states.forEach(function (state) {
- state.addEventListener("click", function () {
-  // Send an HTTP request to the server for the state data
-  fetch(`/${state.id}-data`)
-   .then(response => response.json())
-   .then(data => {
-    // Display the state data on the page
-    document.getElementById("state-name").innerHTML = data[1];
-    document.getElementById("median-price").innerHTML = data[2];
-    document.getElementById("inventory").innerHTML = data[3];
-   });
- });
-});
